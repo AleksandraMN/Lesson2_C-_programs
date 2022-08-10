b85e2d9fa5052b97d0db42e017ebd6e2b20c8239
AleksandraMN <al_mn@mail.ru>
2022-08-10 14:45:00 +1000

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+using System;
+class Program
+{
+	static int SecondPosition(int arg)
+	{
+		int number = arg;
+		int position = -1;
+		if(arg >= 100 && arg < 1000) position = (arg / 10) % 10;
+		return position;
+	}
+	static void AnyNumber()
+	{	
+		Console.Write("Введите целое натуральное трёхзначное число  = ");
+		int number = Convert.ToInt32(Console.ReadLine());
+		int position = SecondPosition(number);
+		if(position > 0)
+		{
+			Console.WriteLine("Вторая цифра этого числа равна = " + position + ".");	
+		} else
+		{
+			Console.WriteLine("Введённое чиcло не является трехзначным целым натуральным числом!");
+		}
+	}
+	static void Main(string[] args)
+	{
+		AnyNumber();
+	}
+}
